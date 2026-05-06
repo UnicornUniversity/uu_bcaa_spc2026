a750fd1ab9eba7d725c6dd325946f06f2f2dfab8
Ivo Milota <149657672+IMilota@users.noreply.github.com>
2026-05-06 10:59:31 +0200

    client

--- server/abl/transaction/listAbl.js
+++ server/abl/transaction/listAbl.js
@@ -9,7 +9,7 @@ const categoryDao = require("../../dao/category-dao.js");
 const schema = {
   type: "object",
   properties: {
-    date: { type: "string", format: "date" },
+    date: { type: "string" },
   },
   required: [],
   additionalProperties: false,
